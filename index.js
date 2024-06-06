a330e5f82c1340934407772665bdcba974dbf2e7
sri ranga <srirangapuram.817@gmail.com>
2024-06-06 13:38:08 +0530

    slokas are attached to the response

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from "express"
-
+import express from "express";
+import slokas from "./slokas.js";
 const app = express();
 const port = 3000;
 
-
+app.get("/", (req, res)=>{
+    res.send(slokas.eightPointFive);
+})
 
 app.listen(port, ()=>{
     console.log(`listening at port number ${port}`);
