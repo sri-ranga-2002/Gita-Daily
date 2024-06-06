0edab6786228eee3935861d3beea2598102353f9
sri ranga <srirangapuram.817@gmail.com>
2024-06-06 13:19:31 +0530

    slokas and resources files added

---
+++ index.js
@@ -0,0 +1,214 @@
+const slokas = {
+    "onePointOne" : "<p><strong>dhṛtarāṣṭra uvāca<br>dharma-kṣetre kuru-kṣetre<br>samavetā yuyutsavaḥ<br>māmakāḥ pāṇḍavāś caiva<br>kim akurvata sañjaya</strong></p>",
+    "twoPointSeven": "<p><strong>kārpaṇya-doṣopahata-svabhāvaḥ<br>pṛcchāmi tvāṁ dharma-sammūḍha-cetāḥ<br>yac chreyaḥ syān niścitaṁ brūhi tan me<br>śiṣyas te 'haṁ śādhi māṁ tvāṁ prapannam</strong></p>",
+    
+    "twoPointEleven": "<p><strong>aśochyān anvaśochas tvaṁ<br>prajñā-vādāṁśh cha bhāṣhase<br>gatāsūn agatāsūṁśh cha<br>nānuśhochanti paṇḍitāḥ</strong></p>",
+
+    "twoPointTwelve": "<p><strong>na tvevāhaṁ jātu nāsaṁ<br>na tvaṁ neme janādhipāḥ<br>na chaiva na bhaviṣhyāmaḥ<br>sarve vayam ataḥ param</strong></p>",
+  
+    "twoPointThirteen": "<p><strong>dehino 'smin yathā dehe<br>kaumāraṁ yauvanaṁ jarā<br>tathā dehāntara-prāptir<br>dhīras tatra na muhyati</strong></p>",
+  
+    "twoPointFourteen": "<p><strong>mātrā-sparśās tu kaunteya<br>śhītoṣhṇa-sukha-duḥkha-dāḥ<br>āgamāpāyino 'nityās<br>tāṁs titikṣhasva bhārata</strong></p>",
+  
+    "twoPointTwenty": "<p><strong>na jāyate mriyate vā kadāchit<br>nāyaṁ bhūtvā bhavitā vā na bhūyaḥ<br>ajo nityaḥ śhāśhvato 'yaṁ purāṇo<br>na hanyate hanyamāne śharīre</strong></p>",
+  
+    "twoPointTwentyTwo": "<p><strong>vāsānsi jīrṇāni yathā vihāya<br>navāni gṛihṇāti naro 'parāṇi<br>tathā śharīrāṇi vihāya jīrṇāny<br>anyāni saṁyāti navāni dehī</strong></p>",
+  
+    "twoPointTwentyThree": "<p><strong>nainaṁ chhindanti śhastrāṇi<br>nainaṁ dahati pāvakaḥ<br>na chainaṁ kledayanty āpo<br>na śhoṣhayati mārutaḥ</strong></p>",
+  
+    "twoPointTwentySeven": "<p><strong>jātasya hi dhruvo mṛityur<br>dhruvaṁ janma mṛitasya cha<br>tasmād aparihārye 'rthe<br>na tvaṁ śhochitum arhasi</strong></p>",
+  
+    "twoPointThirty": "<p><strong>dehī nityam avadhyo 'yaṁ<br>dehe sarvasya bhārata<br>tasmāt sarvāṇi bhūtāni<br>na tvaṁ śhochitum arhasi</strong></p>",
+  
+    "twoPointForty": "<p><strong>nehābhikrama-nāśho 'sti<br>pratyavāyo na vidyate<br>svalpam apyasya dharmasya<br>trāyate mahato bhayāt</strong></p>",
+  
+    "twoPointFortyOne": "<p><strong>vyavasāyātmikā buddhir<br>ekeha kuru-nandana<br>bahu-śhākhā hy anantāśh cha<br>buddhayo 'vyavasāyinām</strong></p>",
+  
+    "twoPointFortyFour": "<p><strong>bhogaiśvarya-prasaktānāṁ<br>tayāpahṛita-chetasām<br>vyavasāyātmikā buddhiḥ<br>samādhau na vidhīyate</strong></p>",
+  
+    "twoPointFortyFive": "<p><strong>traiguṇya-viṣhayā vedā<br>nistraiguṇyo bhavārjuna<br>nirdvandvo nitya-sattva-stho<br>niryoga-kṣhema ātmavān</strong></p>",
+  
+    "twoPointFortySix": "<p><strong>yāvān artha udapāne<br>sarvataḥ samplutodake<br>tāvān sarveṣhu vedeṣhu<br>brāhmaṇasya vijānataḥ</strong></p>",
+  
+    "twoPointFiftyNine": "<p><strong>viṣhayā vinivartante<br>nirāhārasya dehinaḥ<br>rasa-varjaṁ raso 'py asya<br>paraṁ dṛiṣṭvā nivartate</strong></p>",
+  
+    "twoPointSixtyTwo": "<p><strong>dhāyato viṣhayān puṁsaḥ<br>saṅgas teṣhūpajāyate<br>saṅgāt sañjāyate kāmaḥ<br>kāmāt krodho 'bhijāyate</strong></p>",
+  
+    "twoPointSixtyThree": "<p><strong>krodhād bhavati sammohaḥ<br>saṁmohāt smṛiti-vibhramaḥ<br>smṛiti-bhraṁśhād buddhi-nāśho<br>buddhi-nāśhāt praṇaśhyati</strong></p>",
+  
+    "twoPointSixtyFour": "<p><strong>rāga-dveṣha-vimuktais tu<br>viṣhayān indriyaiś charan<br>ātma-vaśhyair vidheyātmā<br>prasādam adhigachchhati</strong></p>",
+  
+    "twoPointSixtyNine": "<p><strong>yā niśhā sarva-bhūtānāṁ<br>tasyāṁ jāgarti saṁyamī<br>yasyāṁ jāgrati bhūtāni<br>sā niśhā paśhyato muneḥ</strong></p>",
+
+    "threePointNine": "<p><strong>yajñārthāt karmaṇo 'nyatra<br>loko 'yaṁ karma-bandhanaḥ<br>tad-arthaṁ karma kaunteya<br>mukta-saṅgaḥ samāchara</strong></p>",
+
+  "threePointFourteen": "<p><strong>annād bhavanti bhūtāni<br>parjanyād anna-sambhavaḥ<br>yajñād bhavati parjanyo<br>yajñaḥ karma-samudbhavaḥ</strong></p>",
+
+  "threePointTwentyOne": "<p><strong>yad yad ācharati śhreṣhṭhas<br>tat tad evetaro janaḥ<br>sa yat pramāṇaṁ kurute<br>lokas tad anuvartate</strong></p>",
+
+  "threePointTwentySeven": "<p><strong>prakṛiteḥ kriyamāṇāni<br>guṇaiḥ karmāṇi sarvaśhaḥ<br>ahaṅkāra-vimūḍhātmā<br>kartāham iti manyate</strong></p>",
+
+  "threePointThirtySeven": "<p><strong>kāma eṣha krodha eṣha<br>rajo-guṇa-samudbhavaḥ<br>mahāśhano mahā-pāpmā<br>viddhy enam iha vairiṇam</strong></p>",
+
+  "fourPointOne": "<p><strong>śrī-bhagavān uvāca<br>imaṁ vivasvate yogaṁ<br>proktavān aham avyayam<br>vivasvān manave prāha<br>manur ikṣhvākave 'bravīt</strong></p>",
+
+  "fourPointTwo": "<p><strong>evaṁ paramparā-prāptam<br>imaṁ rājarṣhayo viduḥ<br>sa kāleneha mahatā<br>yogo naṣhṭaḥ paraṁtapa</strong></p>",
+
+  "fourPointThree": "<p><strong>sa evāyaṁ mayā te 'dya<br>yogaḥ proktaḥ purātanaḥ<br>bhakto 'si me sakhā cheti<br>rahasyaṁ hy etad uttamam</strong></p>",
+
+  "fourPointSix": "<p><strong>ajo 'pi sann avyayātmā<br>bhūtānām īśhvaro 'pi san<br>prakṛitiṁ svām adhiṣhṭhāya<br>saṁbhavāmy ātma-māyayā</strong></p>",
+
+  "fourPointSeven": "<p><strong>yadā yadā hi dharmasya<br>glānir bhavati bhārata<br>abhyutthānam adharmasya<br>tadātmānaṁ sṛijāmy aham</strong></p>",
+
+  "fourPointEight": "<p><strong>paritrāṇāya sādhūnāṁ<br>vināśāya cha duṣhkṛitām<br>dharma-saṁsthāpanārthāya<br>saṁbhavāmi yuge yuge</strong></p>",
+
+  "fourPointNine": "<p><strong>janma karma cha me divyam<br>evaṁ yo vetti tattvataḥ<br>tyaktvā dehaṁ punar janma<br>naiti mām eti so 'rjuna</strong></p>",
+
+  "fourPointTen": "<p><strong>vīta-rāga-bhaya-krodhā<br>man-mayā mām upāśhritāḥ<br>bahavo jñāna-tapasā<br>pūtā mad-bhāvam āgatāḥ</strong></p>",
+
+  "fourPointEleven": "<p><strong>ye yathā māṁ prapadyante<br>tāns tathaiva bhajāmy aham<br>mama vartmānuvartante<br>manuṣhyāḥ pārtha sarvaśhaḥ</strong></p>",
+
+  "fourPointThirteen": "<p><strong>chātur-varṇyaṁ mayā sṛiṣhṭaṁ<br>guṇa-karma-vibhāgaśhaḥ<br>tasya kartāram api māṁ<br>viddhy akartāram avyayam</strong></p>",
+
+  "fourPointThirtyFour": "<p><strong>tad viddhi praṇipātena<br>paripraśhnena sevayā<br>upadekṣhyanti te jñānaṁ<br>jñāninas tattva-darśinaḥ</strong></p>",
+
+  "fivePointEighteen": "<p><strong>vidyā-vinaya-sampanne<br>brāhmaṇe gavi hastini<br>śhuni chaiva śhva-pāke cha<br>paṇḍitāḥ sama-darśhinaḥ</strong></p>",
+
+  "fivePointTwentyTwo": "<p><strong>ye hi saṁsparśha-jā bhogā<br>duḥkha-yonaya eva te<br>ādy-antavantaḥ kaunteya<br>na teṣhu ramate budhaḥ</strong></p>",
+
+  "fivePointTwentyNine": "<p><strong>bhoktāraṁ yajña-tapasāṁ<br>sarva-loka-maheśhvaram<br>suhṛidaṁ sarva-bhūtānāṁ<br>jñātvā māṁ śhāntim ṛichchhati</strong></p>",
+
+  "sixPointSeventeen": "<p><strong>yuktāhāra-vihārasya<br>yukta-cheṣhṭasya karmasu<br>yukta-svapnāvabodhasya<br>yogo bhavati duḥkha-hā</strong></p>",
+
+  "sixPointFortyOne": "<p><strong>prāpya puṇya-kṛitāṁ lokān<br>uṣhitvā śhāśhvatīḥ samāḥ<br>śhuchīnāṁ śhrīmatāṁ gehe<br>yoga-bhraṣhṭo 'bhijāyate</strong></p>",
+
+  "sixPointFortySeven": "<p><strong>yoginām api sarveṣhāṁ<br>mad-gatenāntar-ātmanā<br>śhraddhāvān bhajate yo māṁ<br>sa me yuktatamo mataḥ</strong></p>",
+
+  "sevenPointFour": "<p><strong>bhūmir āpo 'nalo vāyuḥ<br>khaṁ mano buddhir eva cha<br>ahaṅkāra itīyaṁ me<br>bhinnā prakṛitir aṣhṭadhā</strong></p>",
+
+  "sevenPointFive": "<p><strong>apareyam itas tv anyāṁ<br>prakṛitiṁ viddhi me parām<br>jīva-bhūtāṁ mahā-bāho<br>yayedaṁ dhāryate jagat</strong></p>",
+
+  "sevenPointSeven": "<p><strong>mattaḥ parataraṁ nānyat<br>kiñchid asti dhanañjaya<br>mayi sarvam idaṁ protaṁ<br>sūtre maṇi-gaṇā iva</strong></p>",
+
+  "sevenPointFourteen": "<p><strong>daivī hy eṣhā guṇa-mayī<br>mama māyā duratyayā<br>mām eva ye prapadyante<br>māyām etāṁ taranti te</strong></p>",
+
+  "sevenPointFifteen": "<p><strong>na māṁ duṣhkṛitino mūḍhāḥ<br>prapadyante narādhamāḥ<br>māyayāpahṛita-jñānā<br>āsuraṁ bhāvam āśhritāḥ</strong></p>",
+
+  "sevenPointSixteen": "<p><strong>chatur-vidhā bhajante māṁ<br>janāḥ sukṛitino 'rjuna<br>ārto jijñāsur arthārthī<br>jñānī cha bharatarṣhabha</strong></p>",
+
+  "sevenPointNineteen": "<p><strong>bahūnāṁ janmanām ante<br>jñānavān māṁ prapadyate<br>vāsudevaḥ sarvam iti<br>sa mahātmā su-durlabhaḥ</strong></p>",
+
+  "sevenPointTwentyFive": "<p><strong>nāhaṁ prakāśhaḥ sarvasya<br>yoga-māyā-samāvṛitaḥ<br>mūḍho 'yaṁ nābhijānāti<br>loko mām ajam avyayam</strong></p>",
+
+  "sevenPointTwentySix": "<p><strong>vedāhaṁ samatītāni<br>vartamānāni chārjuna<br>bhaviṣhyāṇi cha bhūtāni<br>māṁ tu veda na kaśhchana</strong></p>",
+
+  "sevenPointTwentySeven": "<p><strong>ichchhā-dveṣha-samutthena<br>dvandva-mohena bhārata<br>sarva-bhūtāni saṁmohaṁ<br>sarge yānti parantapa</strong></p>",
+
+  "sevenPointTwentyEight": "<p><strong>yeṣhāṁ tv anta-gataṁ pāpaṁ<br>janānāṁ puṇya-karmaṇām<br>te dvandva-moha-nirmuktā<br>bhajante māṁ dṛiḍha-vratāḥ</strong></p>",
+
+  "eightPointFive": "<p><strong>anta-kāle cha mām eva<br>smaran muktvā kalevaram<br>yaḥ prayāti sa mad-bhāvaṁ<br>yāti nāsty atra sanśayaḥ</strong></p>",
+
+  "eightPointSix": "<p><strong>yaṁ yaṁ vāpi smaran bhāvaṁ<br>tyajaty ante kalevaram<br>taṁ tam evaiti kaunteya<br>sadā tad-bhāva-bhāvitaḥ</strong></p>",
+
+  "eightPointSeven": "<p><strong>tasmāt sarveṣhu kāleṣhu<br>mām anusmara yudhya cha<br>mayy arpita-mano-buddhir<br>mām evaiṣhyasy asaṁśayaḥ</strong></p>",
+
+  "eightPointFourteen": "<p><strong>ananya-chetāḥ satataṁ<br>yo māṁ smarati nityaśaḥ<br>tasyāhaṁ sulabhaḥ pārtha<br>nitya-yuktasya yoginaḥ</strong></p>",
+
+  "eightPointFifteen": "<p><strong>mām upetya punar janma<br>duḥkhālayaṁ aśhāśhvatam<br>nāpnuvanti mahātmānaḥ<br>saṁsiddhiṁ paramāṁ gatāḥ</strong></p>",
+
+  "eightPointSixteen": "<p><strong>ā-brahma-bhuvanāl lokāḥ<br>punar āvartino 'rjuna<br>mām upetya tu kaunteya<br>punar janma na vidyate</strong></p>",
+
+  "eightPointTwentyEight": "<p><strong>vedeṣhu yajñeṣhu tapaḥsu chaiva<br>dāneṣhu yat puṇya-phalaṁ pradiṣṭam<br>atyeti tat sarvam idaṁ viditvā<br>yogī paraṁ sthānam upaiti chādyam</strong></p>",
+
+  "ninePointTwo": "<p><strong>rāja-vidyā rāja-guhyaṁ<br>pavitram idam uttamam<br>pratyakṣhāvagamaṁ dharmyaṁ<br>susukhaṁ kartum avyayam</strong></p>",
+
+  "ninePointFour": "<p><strong>mayā tatam idaṁ sarvaṁ<br>jagad avyakta-mūrtinā<br>mat-sthāni sarva-bhūtāni<br>na chāhaṁ teṣhv avasthitaḥ</strong></p>",
+
+  "ninePointTen": "<p><strong>mayādhyakṣeṇa prakṛitiḥ<br>sūyate sa-chara-acharam<br>hetunānena kaunteya<br>jagad viparivartate</strong></p>",
+
+  "ninePointEleven": "<p><strong>avajānanti māṁ mūḍhā<br>mānuṣhīṁ tanum āśhritam<br>paraṁ bhāvam ajānanto<br>mama bhūta-maheśvaram</strong></p>",
+
+  "ninePointTwelve": "<p><strong>moghāśhā mogha-karmāṇo<br>mogha-jñānā vichetasaḥ<br>rākṣhasīm āsurīṁ chaiva<br>prakṛitiṁ mohinīṁ śhritāḥ</strong></p>",
+
+  "ninePointThirteen": "<p><strong>mahātmānas tu māṁ pārtha<br>daivīṁ prakṛitim āśhritāḥ<br>bhajanty ananya-manaso<br>jñātvā bhūtādim avyayam</strong></p>",
+
+  "ninePointFourteen": "<p><strong>satataṁ kīrtayanto māṁ<br>yatantaśh cha dṛiḍha-vratāḥ<br>namasyantaśh cha māṁ bhaktyā<br>nitya-yuktā upāsate</strong></p>",
+
+  "ninePointTwentyTwo": "<p><strong>ananyāśh chintayanto māṁ<br>ye janāḥ paryupāsate<br>teṣhāṁ nityābhiyuktānāṁ<br>yoga-kṣhemaṁ vahāmy aham</strong></p>",
+
+  "ninePointTwentyFive": "<p><strong>yānti deva-vratā devān<br>pitṝn yānti pitṛi-vratāḥ<br>bhūtāni yānti bhūtejyā<br>yānti mad-yājino 'pi mām</strong></p>",
+
+  "ninePointTwentySix": "<p><strong>patraṁ puṣhpaṁ phalaṁ toyaṁ<br>yo me bhaktyā prayachchhati<br>tad ahaṁ bhakty-upahṛitam<br>aśhnāmi prayatātmanaḥ</strong></p>",
+
+  "ninePointTwentySeven": "<p><strong>yat karoṣhi yad aśhnāsi<br>yaj juhoṣhi dadāsi yat<br>yat tapasyasi kaunteya<br>tat kuruṣhva mad-arpaṇam</strong></p>",
+
+  "ninePointTwentyNine": "<p><strong>samo 'haṁ sarva-bhūteṣhu<br>na me dveṣhyo 'sti na priyaḥ<br>ye bhajanti tu māṁ bhaktyā<br>mayi te teṣhu chāpy aham</strong></p>",
+
+  "ninePointThirty": "<p><strong>api chet su-durāchāro<br>bhajate mām ananya-bhāk<br>sādhur eva sa mantavyaḥ<br>samayag vyavasito hi saḥ</strong></p>",
+
+  "ninePointThirtyTwo": "<p><strong>māṁ hi pārtha vyapāśhritya<br>ye 'pi syuḥ pāpa-yonayaḥ<br>striyo vaiśhyās tathā śhūdrās<br>te 'pi yānti parāṁ gatim</strong></p>",
+
+  "ninePointThirtyFour": "<p><strong>man-manā bhava mad-bhakto<br>mad-yājī māṁ namaskuru<br>mām evaiṣhyasi yuktvaivam<br>ātmānaṁ mat-parāyaṇaḥ</strong></p>",
+
+  "tenPointEight": "<p><strong>ahaṁ sarvasya prabhavo<br>mattaḥ sarvaṁ pravartate<br>iti matvā bhajante māṁ<br>budhā bhāva-samanvitāḥ</strong></p>",
+
+  "tenPointNine": "<p><strong>mach-chittā mad-gata-prāṇā<br>bodhayantaḥ parasparam<br>kathayantaśh cha māṁ nityaṁ<br>tuṣhyanti cha ramanti cha</strong></p>",
+
+  "tenPointTen": "<p><strong>teṣhāṁ satata-yuktānāṁ<br>bhajatāṁ prīti-pūrvakam<br>dadāmi buddhi-yogaṁ taṁ<br>yena mām upayānti te</strong></p>",
+
+  "tenPointEleven": "<p><strong>teṣhām evānukampārtham<br>aham ajñāna-jaṁ tamaḥ<br>nāśhayāmy ātma-bhāvastho<br>jñāna-dīpena bhāsvatā</strong></p>",
+
+  "tenPointTwelve": "<p><strong>arjuna uvācha<br>paraṁ brahma paraṁ dhāma<br>pavitraṁ paramaṁ bhavān<br>puruṣhaṁ śhāśvataṁ divyam<br>ādi-devam ajaṁ vibhum</strong></p>",
+
+  "tenPointThirteen": "<p><strong>āhus tvām ṛiṣhayaḥ sarve<br>devarṣhir nāradas tathā<br>asito devalo vyāsaḥ<br>svayaṁ chaiva bravīṣhi me</strong></p>",
+
+  "tenPointFortyOne": "<p><strong>yad yad vibhūtimat sattvaṁ<br>śhrīmad ūrjitam eva vā<br>tat tad evāvagachcha tvaṁ<br>mama tejo 'ṁśha-sambhavam</strong></p>",
+
+  "elevenPointFiftyFour": "<p><strong>bhaktyā tvananyayā śhakya<br>aham evaṁ-vidho 'rjuna<br>jñātuṁ draṣṭuṁ cha tattvena<br>praveṣṭuṁ cha paraṁ-tapa</strong></p>",
+  "elevenPointFiftyFive": "<p><strong>mat-karma-kṛin mat-paramo<br>mad-bhaktaḥ saṅga-varjitaḥ<br>nirvairaḥ sarva-bhūteṣhu<br>yaḥ sa māmeti pāṇḍava</strong></p>",
+
+  "twelvePointFive": "<p><strong>kleśho 'dhikataras teṣhām<br>avyaktāsakta-chetasām<br>avyaktā hi gatir duḥkhaṁ<br>dehavadbhir avāpyate</strong></p>",
+
+  "twelvePointEight": "<p><strong>mayy eva mana ādhatsva<br>mayi buddhiṁ niveśhaya<br>nivasiṣhyasi mayy eva<br>ata ūrdhvaṁ na sanśhayaḥ</strong></p>",
+
+  "twelvePointNine": "<p><strong>atha chittaṁ samādhātuṁ<br>na śhaknoṣhi mayi sthiram<br>abhyāsa-yogena tato<br>mām ichāptuṁ dhanañjaya</strong></p>",
+
+  "twelvePointTen": "<p><strong>abhyāse 'py asamartho 'si<br>mat-karma-paramo bhava<br>mad-artham api karmāṇi<br>kurvan siddhim avāpsyasi</strong></p>",
+
+  "fourteenPointFour": "<p><strong>sarva-yoniṣhu kaunteya<br>mūrtayaḥ sambhavanti yāḥ<br>tāsāṁ brahma mahad yonir<br>ahaṁ bīja-pradaḥ pitā</strong></p>",
+
+  "fourteenPointTwentySix": "<p><strong>māṁ cha yo 'vyabhichāreṇa<br>bhakti-yogena sevate<br>sa guṇān samatītyaitān<br>brahma-bhūyāya kalpate</strong></p>",
+
+  "fourteenPointTwentySeven": "<p><strong>brahmaṇo hi pratiṣhṭhāham<br>amṛitasyāvyayasya cha<br>śhāśhvatasya cha dharmasya<br>sukhasyaikāntikasya cha</strong></p>",
+
+  "fifteenPointFive": "<p><strong>nirmāna-mohā jita-saṅga-doṣhā<br>adhyātma-nityā vinivṛitta-kāmāḥ<br>dvandvair vimuktāḥ sukha-duḥkha-saṁjñair<br>gachchhanty amūḍhāḥ padam avyayaṁ tat</strong></p>",
+
+  "fifteenPointSix": "<p><strong>na tad bhāsayate sūryo<br>na śhaśhāṅko na pāvakaḥ<br>yad gatvā na nivartante<br>tad dhāma paramaṁ mama</strong></p>",
+
+  "fifteenPointSeven": "<p><strong>mamaivāṁśho jīva-loke<br>jīva-bhūtaḥ sanātanaḥ<br>manaḥ-ṣhaṣhṭhānīndriyāṇi<br>prakṛiti-sthāni karṣhati</strong></p>",
+
+  "fifteenPointFifteen": "<p><strong>sarvasya chāhaṁ hṛidi sanniviṣhṭo<br>mattaḥ smṛitir jñānam apohanaṁ cha<br>vedaiśhcha sarvair aham eva vedyo<br>vedānta-kṛid veda-vid eva chāham</strong></p>",
+
+  "fifteenPointNineteen": "<p><strong>yo mām evam asammūḍho<br>jānāti puruṣhottamam<br>sa sarva-vid bhajati māṁ<br>sarva-bhāvena bhārata</strong></p>",
+
+  "eighteenPointFortyTwo": "<p><strong>śhāuryaṁ tejo dhṛitir dākṣhyaṁ<br>yuddhe chāpy apalāyanam<br>dānam īśhvara-bhāvaśh cha<br>kṣhatraṁ karma svabhāva-jam</strong></p>",
+
+  "eighteenPointFiftyFour": "<p><strong>brahma-bhūtaḥ prasannātmā<br>na śhochati na kāṅkṣhati<br>ṣhamas sarveṣhu bhūteṣhu<br>mad-bhaktiṁ labhate parām</strong></p>",
+
+  "eighteenPointFiftyFive": "<p><strong>bhaktyā mām abhijānāti<br>yāvān yaśh chāsmi tattvataḥ<br>tato māṁ tattvato jñātvā<br>viśhate tad-anantaram</strong></p>",
+
+  "eighteenPointFiftyEight": "<p><strong>mach-chittaḥ sarva-durgāṇi<br>mat-prasādāt tariṣhyasi<br>atha chet tvam ahaṅkārān<br>na śhroṣhyasi vinaṅkṣhyasi</strong></p>",
+
+  "eighteenPointSixtyOne": "<p><strong>īśhvaraḥ sarva-bhūtānāṁ<br>hṛid-deśhe 'rjuna tiṣhṭhati<br>bhrāmayan sarva-bhūtāni<br>yantrārūḍhāni māyayā</strong></p>",
+
+  "eighteenPointSixtyFive": "<p><strong>man-manā bhava mad-bhakto<br>mad-yājī māṁ namaskuru<br>mām evaiṣhyasi satyaṁ te<br>pratijāne priyo 'si me</strong></p>",
+
+  "eighteenPointSixtySix": "<p><strong>sarva-dharmān parityajya<br>mām ekaṁ śharaṇaṁ vraja<br>ahaṁ tvāṁ sarva-pāpebhyo<br>mokṣhayiṣhyāmi mā śhuchaḥ</strong></p>",
+
+  "eighteenPointSixtyEight": "<p><strong>ya idaṁ paramaṁ guhyaṁ<br>mad-bhakteṣhv abhidhāsyati<br>bhaktiṁ mayi parāṁ kṛitvā<br>mām evaiṣhyaty asaṁśhayaḥ</strong></p>",
+
+  "eighteenPointSixtyNine": "<p><strong>na cha tasmān manuṣhyeṣhu<br>kaśhchin me priya-kṛittamaḥ<br>bhavitā na cha me tasmād<br>anyaḥ priyataro bhuvi</strong></p>",
+
+  "eighteenPointSeventyEight": "<p><strong>yatra yogeśhvaraḥ kṛiṣhṇo<br>yatra pārtho dhanur-dharaḥ<br>tatra śhrīr vijayo bhūtir<br>dhruvā nītir matir mama</strong></p>"
+    };
+console.log(slokas.elevenPointFiftyFive);
